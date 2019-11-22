86dedea9212e6a507310f32d4e04ad62ad6b4d77
William <williamphenicie@outlook.com>
2019-11-22 13:36:00 -0600

    Trying to fix smal turns and stuff like that

--- MazeChallenge.c
+++ MazeChallenge.c
@@ -11,20 +11,20 @@ void resetMotor() {
 	motor[armMotor] = 0;
 }
 
-void turnRight(int speed, int waitTime) {
+void turnLeft(int speed, int waitTime) {
 	//movingForward = 1;
 	wait1Msec(1000);
-	motor[rightMotor] = 0;
+	motor[rightMotor] = speed*-1;
 	motor[leftMotor] = speed;
 	wait1Msec(waitTime);
 	resetMotor();
 }
 
-void turnLeft(int speed, int waitTime) {
+void turnRight(int speed, int waitTime) {
 	//movingForward = 1;
 	wait1Msec(1000);
 	motor[rightMotor] = speed;
-	motor[leftMotor] = 0;
+	motor[leftMotor] = speed*-1;
 	wait1Msec(waitTime);
 	resetMotor();
 }
@@ -71,6 +71,41 @@ void moveForward(int speed, int waitTime) {
 
 task main()
 {
+
+//Heading to pickup ball
+	openClaw();
+	moveArmUp(25, 250);
+	moveForward(90,900);
+  turnLeft(50, 1750);
+	moveForward(90,2300);
+	turnRight(50,1750);
+	moveForward(50,1500);
+	turnRight(50,1800);
+	moveForward(70,900);
+	closeClaw();
+	moveArmUp(50, 250);
+
+	//Picked Up Ball, next step
+	moveForward(-70,900);
+	turnRight(50,1450);
+	moveForward(50,2300);
+	turnLeft(50,1800);
+	moveForward(90,1000);
+	turnLeft(50,1800);
+	moveForward(90,1000);
+	turnLeft(50,1800);
+	moveForward(70,900);
+	turnRight(50,1750);
+	moveForward(50,1000);
+	turnRight(50,1750);
+	moveForward(70,800);
+	openClaw();
+
+
+
+
+
+
 
 
 
