983d74c65e101192e8d7576152dc2786d3a7392e
William <williamphenicie@outlook.com>
2019-12-09 10:04:14 -0600

    Finished Maze Challenge Product

--- MazeChallenge.c
+++ MazeChallenge.c
@@ -3,12 +3,28 @@
 #pragma config(Motor,  port7,           armMotor,      tmotorVex393, openLoop, reversed)
 #pragma config(Motor,  port10,          rightMotor,    tmotorVex393, openLoop)
 
-short movingForward = false;
+short movingForward =  false;
+short holdArm = false;
 
 void resetMotor() {
 	motor[rightMotor] = 0;
 	motor[leftMotor] = 0;
+	if (holdArm == false) {
 	motor[armMotor] = 0;
+}
+}
+
+void moveArmUp(int speed, int waitTime) {
+	wait1Msec(1000);
+	motor[armMotor] = speed;
+	wait1Msec(waitTime);
+	resetMotor();
+}
+
+void holdArmAt(int speed, int waitTime) {
+		holdArm = true;
+		moveArmUp(speed, waitTime);
+		motor[armMotor] = 20;
 }
 
 void turnLeft(int speed, int waitTime) {
@@ -29,12 +45,7 @@ void turnRight(int speed, int waitTime) {
 	resetMotor();
 }
 
-void moveArmUp(int speed, int waitTime) {
-	wait1Msec(1000);
-	motor[armMotor] = speed;
-	wait1Msec(waitTime);
-	resetMotor();
-}
+
 
 void moveArmDown(int speed, int waitTime) {
 	wait1Msec(1000);
@@ -76,29 +87,30 @@ task main()
 	openClaw();
 	moveArmUp(25, 250);
 	moveForward(90,900);
-  turnLeft(50, 1750);
-	moveForward(90,2300);
-	turnRight(50,1750);
-	moveForward(50,1500);
-	turnRight(50,1800);
-	moveForward(70,900);
+  turnLeft(50, 1600);
+	moveForward(90,2400);
+	turnRight(50,1500);
+	moveForward(90,1250);
+	turnRight(50,1500);
+	moveForward(70,1000);
 	closeClaw();
-	moveArmUp(50, 250);
+
+	holdArmAt(50, 700);
 
 	//Picked Up Ball, next step
 	moveForward(-70,900);
+	turnRight(50,1400);
+	moveForward(50,2100);
+	turnLeft(50,1450);
+	moveForward(90,2400);
+	turnLeft(50,1500);
+	moveForward(90,2500);
+	turnLeft(50,1600);
+	moveForward(70,3000);
 	turnRight(50,1450);
-	moveForward(50,2300);
-	turnLeft(50,1800);
-	moveForward(90,1000);
-	turnLeft(50,1800);
-	moveForward(90,1000);
-	turnLeft(50,1800);
-	moveForward(70,900);
-	turnRight(50,1750);
-	moveForward(50,1000);
-	turnRight(50,1750);
-	moveForward(70,800);
+	moveForward(50,2000);
+	turnRight(50,1400);
+	moveForward(70,3300);
 	openClaw();
 
 
